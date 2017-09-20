bd360a47b50d268c598bf284e825efecd9fedac8
Alexander <strannik11.92@gmail.com>
2017-09-20 12:41:42 +0300

    Add basic city search

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 $( document ).ready(function(){
-	geoFindMe();
+	// getWeatherData();
+	// geoFindMe();
 });
 
 // Get latitude and longitude
@@ -22,22 +23,47 @@ $( document ).ready(function(){
 	}
 
 // Get data from FreeCodeCamp API
-	function getWeatherData (latitude, longitude) {
-		var urlString = "https://fcc-weather-api.glitch.me/api/current?" + "lat=" + latitude + "&" + "lon=" + longitude;
-		console.log(urlString);
-		$.ajax({
+	// function getWeatherData (latitude, longitude) {
+	// 	var urlString = "https://fcc-weather-api.glitch.me/api/current?" + "lat=" + latitude + "&" + "lon=" + longitude;
+	// 	console.log(urlString);
+	// 	$.ajax({
+	// 		"url" : urlString,
+	// 		"type" : "GET",
+	// 		"success" : function(responce) {
+	// 			console.log(responce);
+	// 			console.log(responce.weather[0]);
+	// 			console.log(responce.weather[0].main);
+	// 			$("body").append("<img src = " + responce.weather[0].icon + "\"" + "</img>");
+	// 		},
+	// 		"error" : function(error) {
+	// 			console.log(error);
+	// 		},
+	// 		crossDomain: true
+	// 	});
+	// 		console.log("lat" + latitude + "&" + "lon" +longitude);
+	// }
+
+function getWeatherData(city) {
+	// var urlString = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=metric&lang=ru&APPID=303c4b17f85d64176ffd592811fd1b91";
+	var urlString = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&lang=ru&APPID=303c4b17f85d64176ffd592811fd1b91";
+	$.ajax({
 			"url" : urlString,
 			"type" : "GET",
 			"success" : function(responce) {
-				console.log(responce);
-				console.log(responce.weather[0]);
-				console.log(responce.weather[0].main);
-				$("body").append("<img src = " + responce.weather[0].icon + "\"" + "</img>");
+				console.log(responce.name);
 			},
 			"error" : function(error) {
 				console.log(error);
 			},
 			crossDomain: true
-		});
-			console.log("lat" + latitude + "&" + "lon" +longitude);
-	}
+	});
+	console.log(urlString);
+	// console.log("lat" + latitude + "&" + "lon" +longitude);
+}
+
+$("#searchCity").on("click", function () {
+	event.preventDefault();
+	// alert($("#input-city").val());
+	var city = $("#input-city").val();
+	getWeatherData(city);
+});
