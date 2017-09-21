ab291cd031270f02c890fec058a78bb5497ee4d1
Alexander <strannik11.92@gmail.com>
2017-09-21 15:35:34 +0300

    Add icons of current weather

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
-$( document ).ready(function(){
+//= partials/date.js
+$(document).ready(function(){
 	// getWeatherData();
 	geoFindMe();
 });
@@ -23,14 +24,14 @@ $( document ).ready(function(){
 	}
 
 function getWeatherDataAuto(latitude, longitude) {
-	var urlString = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&units=metric&lang=ru&APPID=303c4b17f85d64176ffd592811fd1b91";
+	var urlString = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=metric&lang=ru&APPID=303c4b17f85d64176ffd592811fd1b91";
 	getData(urlString);
 	console.log(urlString);
 	// console.log("lat" + latitude + "&" + "lon" +longitude);
 }
 
 function getWeatherData(city) {
-	var urlString = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=metric&lang=ru&APPID=303c4b17f85d64176ffd592811fd1b91";
+	var urlString = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&lang=ru&APPID=303c4b17f85d64176ffd592811fd1b91";
 	getData(urlString);
 	console.log(urlString);
 	// console.log("lat" + latitude + "&" + "lon" +longitude);
@@ -43,12 +44,79 @@ function getData(urlString) {
 			"success" : function(responce) {
 				console.log(responce);
 				console.log(responce.name);
+				console.log(hey(responce));
+				getCityName(responce);
 			},
 			"error" : function(error) {
 				console.log(error);
 			},
 			crossDomain: true
 	});
+}
+
+function hey(responce) {
+	var date = new Date(responce.dt * 1000);
+	return date.toString("M/d/yyyy/HH:mm");
+}
+
+function getCityName(responce) {
+	$("#city-name").text(responce.name);
+	$("#country").text(responce.sys.country);
+	$("#temp").text(Math.floor(responce.main.temp) + String.fromCharCode(176));
+	$("#desc").text(responce.weather[0].main);
+	console.log(responce.weather[0].icon);
+	switch(responce.weather[0].icon) {
+		case "01d":
+			$("#icon-img").attr("src", "../img/animated-icons/day.svg");
+			break;
+		case "01n":
+			$("#icon-img").attr("src", "../img/animated-icons/night.svg");
+			break;
+		case "02d":
+			$("#icon-img").attr("src", "../img/animated-icons/cloudy-day-2.svg");
+			break;
+		case "02n":
+			$("#icon-img").attr("src", "../img/animated-icons/cloudy-night-2.svg");
+			break;
+		case "03d":
+			$("#icon-img").attr("src", "../img/animated-icons/cloudy-day-3.svg");
+			break;
+		case "03n":
+			$("#icon-img").attr("src", "../img/animated-icons/cloudy-night-3.svg");
+			break;
+		case "04d":
+			$("#icon-img").attr("src", "../img/animated-icons/cloudy.svg");
+			break;
+		case "04n":
+			$("#icon-img").attr("src", "../img/animated-icons/cloudy.svg");
+			break;
+		case "09d":
+			$("#icon-img").attr("src", "../img/animated-icons/rainy-2.svg");
+			break;
+		case "09n":
+			$("#icon-img").attr("src", "../img/animated-icons/rainy-5.svg");
+			break;
+		case "10d":
+			$("#icon-img").attr("src", "../img/animated-icons/rainy-3.svg");
+			break;
+		case "10n":
+			$("#icon-img").attr("src", "../img/animated-icons/rainy-6.svg");
+			break;
+		case "11d":
+			$("#icon-img").attr("src", "../img/animated-icons/thunder.svg");
+			break;
+		case "11n":
+			$("#icon-img").attr("src", "../img/animated-icons/thunder.svg");
+			break;
+		case "13d":
+			$("#icon-img").attr("src", "../img/animated-icons/snowy-2.svg");
+			break;
+		case "13n":
+			$("#icon-img").attr("src", "../img/animated-icons/snowy-6.svg");
+			break;
+		default:
+			$("#icon-img").attr("src", "../img/animated-icons/cloudy.svg");
+	}
 }
 
 $("#searchCity").on("click", function () {
