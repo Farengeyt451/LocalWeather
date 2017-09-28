78304d7729f92141fc3d0ea046926c2fad0471a3
Alexander <strannik11.92@gmail.com>
2017-09-28 09:49:52 +0300

    Add search tooltips & auto search loc-n on openweathermap

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-	// getWeatherData();
 	geoFindMe();
 });
 
@@ -23,20 +22,30 @@ $(document).ready(function(){
 		navigator.geolocation.getCurrentPosition(success, error);
 	}
 
+// Search location manually
+$("#searchCity").on("click", function () {
+	event.preventDefault();
+	var city = $("#input-city").val();
+	getWeatherData(city);
+});
+
+// Get weather data based on latitude and longitude
 function getWeatherDataAuto(latitude, longitude) {
-	var urlString = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=metric&lang=ru&APPID=303c4b17f85d64176ffd592811fd1b91";
+	var urlString = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=metric&lang=en&APPID=303c4b17f85d64176ffd592811fd1b91";
 	getData(urlString);
 	console.log(urlString);
 	// console.log("lat" + latitude + "&" + "lon" +longitude);
 }
 
+// Get weather data based on search
 function getWeatherData(city) {
-	var urlString = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&lang=ru&APPID=303c4b17f85d64176ffd592811fd1b91";
+	var urlString = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&lang=en&APPID=303c4b17f85d64176ffd592811fd1b91";
 	getData(urlString);
 	console.log(urlString);
 	// console.log("lat" + latitude + "&" + "lon" +longitude);
 }
 
+// Get weather AJAX
 function getData(urlString) {
 		$.ajax({
 			"url" : urlString,
@@ -46,7 +55,7 @@ function getData(urlString) {
 				console.log(responce.name);
 				console.log("Rise " + transformDate(responce.sys.sunrise));
 				console.log("Set " + transformDate(responce.sys.sunset));
-				getCityName(responce);
+				appendData(responce);
 			},
 			"error" : function(error) {
 				console.log(error);
@@ -55,76 +64,83 @@ function getData(urlString) {
 	});
 }
 
+// Convert Date from ms
+// TODO Don`t use library time.js
 function transformDate(responce) {
 	var timeSunriseSunstet = new Date(responce * 1000);
 	return timeSunriseSunstet.toString("HH:mm");
 }
 
-function getCityName(responce) {
-	$("#city-name").text(responce.name);
-	$("#country").text(responce.sys.country);
-	$("#desc").text(responce.weather[0].description);
-	$("#temp").text(Math.floor(responce.main.temp) + String.fromCharCode(176) + "C");
-	$("#humidity").text(responce.main.humidity + String.fromCharCode(37));
-	$("#pressure").text(Math.floor(responce.main.pressure * 0.750062) + " " + "мм рт.ст");
-	$("#wind").text(responce.wind.speed + " " + "м/с");
+// Append data to html
+function appendData(responce) {
+	var nameCity = responce.name;
+	var nameCountry = responce.sys.country;
+	var weatherDesc = responce.weather[0].description;
+	var infoTemp = responce.main.temp;
+	var infoHumidity = responce.main.humidity;
+	var infoPresure = responce.main.pressure;
+	var infoWind = responce.wind.speed;
+	var typeIcon = responce.weather[0].icon;
+	$("#city-name").text(nameCity);
+	$("#country").text(nameCountry);
+	$("#desc").text(weatherDesc);
+	$("#temp").text(Math.floor(infoTemp) + String.fromCharCode(176) + "C");
+	$("#humidity").text(infoHumidity + String.fromCharCode(37));
+	$("#pressure").text(Math.floor(infoPresure * 0.750062) + " " + "mm of mercury");
+	$("#wind").text(infoWind + " " + "m/s");
+	$("#linkToMap").attr("href", "http://openweathermap.org/find?q=" + nameCity + "," + nameCountry);
 	console.log(responce.weather[0].icon);
-	switch(responce.weather[0].icon) {
+	switch(typeIcon) {
 		case "01d":
-			$("#icon-img").attr("src", "../img/animated-icons/day.svg");
+			$("#icon-img").attr("src", "img/animated-icons/day.svg");
 			break;
 		case "01n":
-			$("#icon-img").attr("src", "../img/animated-icons/night.svg");
+			$("#icon-img").attr("src", "img/animated-icons/night.svg");
 			break;
 		case "02d":
-			$("#icon-img").attr("src", "../img/animated-icons/cloudy-day-2.svg");
+			$("#icon-img").attr("src", "img/animated-icons/cloudy-day-2.svg");
 			break;
 		case "02n":
-			$("#icon-img").attr("src", "../img/animated-icons/cloudy-night-2.svg");
+			$("#icon-img").attr("src", "img/animated-icons/cloudy-night-2.svg");
 			break;
 		case "03d":
-			$("#icon-img").attr("src", "../img/animated-icons/cloudy-day-3.svg");
+			$("#icon-img").attr("src", "img/animated-icons/cloudy-day-3.svg");
 			break;
 		case "03n":
-			$("#icon-img").attr("src", "../img/animated-icons/cloudy-night-3.svg");
+			$("#icon-img").attr("src", "img/animated-icons/cloudy-night-3.svg");
 			break;
 		case "04d":
-			$("#icon-img").attr("src", "../img/animated-icons/cloudy.svg");
+			$("#icon-img").attr("src", "img/animated-icons/cloudy.svg");
 			break;
 		case "04n":
-			$("#icon-img").attr("src", "../img/animated-icons/cloudy.svg");
+			$("#icon-img").attr("src", "img/animated-icons/cloudy.svg");
 			break;
 		case "09d":
-			$("#icon-img").attr("src", "../img/animated-icons/rainy-2.svg");
+			$("#icon-img").attr("src", "img/animated-icons/rainy-2.svg");
 			break;
 		case "09n":
-			$("#icon-img").attr("src", "../img/animated-icons/rainy-5.svg");
+			$("#icon-img").attr("src", "img/animated-icons/rainy-5.svg");
 			break;
 		case "10d":
-			$("#icon-img").attr("src", "../img/animated-icons/rainy-3.svg");
+			$("#icon-img").attr("src", "img/animated-icons/rainy-3.svg");
 			break;
 		case "10n":
-			$("#icon-img").attr("src", "../img/animated-icons/rainy-6.svg");
+			$("#icon-img").attr("src", "img/animated-icons/rainy-6.svg");
 			break;
 		case "11d":
-			$("#icon-img").attr("src", "../img/animated-icons/thunder.svg");
+			$("#icon-img").attr("src", "img/animated-icons/thunder.svg");
 			break;
 		case "11n":
-			$("#icon-img").attr("src", "../img/animated-icons/thunder.svg");
+			$("#icon-img").attr("src", "img/animated-icons/thunder.svg");
 			break;
 		case "13d":
-			$("#icon-img").attr("src", "../img/animated-icons/snowy-2.svg");
+			$("#icon-img").attr("src", "img/animated-icons/snowy-2.svg");
 			break;
 		case "13n":
-			$("#icon-img").attr("src", "../img/animated-icons/snowy-6.svg");
+			$("#icon-img").attr("src", "img/animated-icons/snowy-6.svg");
 			break;
 		default:
-			$("#icon-img").attr("src", "../img/animated-icons/cloudy.svg");
+			$("#icon-img").attr("src", "img/animated-icons/cloudy.svg");
 	}
 }
 
-$("#searchCity").on("click", function () {
-	event.preventDefault();
-	var city = $("#input-city").val();
-	getWeatherData(city);
-});
